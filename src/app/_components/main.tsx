6769df52a708cf392d1411f9e5efb7345cb1e2e2
neemapssree <145227095+neemapssree@users.noreply.github.com>
2025-10-18 15:26:38 +0400

    fixed iphone issues 1

--- src/app/_components/main.tsx
+++ src/app/_components/main.tsx
@@ -13,17 +13,7 @@ export default function Main() {
             <div
             className="relative isolate overflow-hidden pt-14 pb-16 sm:pb-20 bg-[url('/main-banner.png')] bg-cover bg-center bg-no-repeat"
             >
-                <div className="absolute inset-0 bg-gradient-to-r from-black/90 via-black/30 to-transparent" />
-                {/* <img
-                    alt="psi-list" title="psi-list"
-                    src="/main-banner.png"
-                    className="absolute inset-0 w-full h-full opacity-10 z-0 object-cover dark:hidden"
-                />
-                <img
-                    alt="psi-list" title="psi-list"
-                    src="/main-banner.png"
-                    className="absolute inset-0 w-full h-full object-cover opacity-10 hidden dark:block z-0"
-                /> */}
+                <div className="absolute inset-0 bg-gradient-to-r from-black/90 via-black/30 to-transparent" />                
                 <div className="lg:pt-30">
                     <div className="mx-auto container px-6 md:px-10">
                     <div className=" max-w-3xl pt-32 sm:pt-48 relative flex flex-col space-y-5 items-start w-full xl:p-0 lg:py-5 lg:px-1 py-5">
